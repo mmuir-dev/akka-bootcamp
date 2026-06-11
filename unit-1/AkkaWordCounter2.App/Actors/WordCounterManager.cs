d36e67652f9ced930ada426edb52b6c58579ec93
Maxwell Muir <maxwell.muir@bluetread.com>
2026-06-11 11:51:57 -0700

    Completed course and added .gitattributes

---
+++ unit-1/AkkaWordCounter2.App/Actors/WordCounterManager.cs
@@ -0,0 +1,20 @@
+﻿namespace AkkaWordCounter2.App.Actors;
+
+public sealed class WordCounterManager : ReceiveActor
+{
+    public WordCounterManager()
+    {
+        Receive<IWithDocumentId>(s =>
+        {
+            string childName = $"word-counter-{s.DocumentId.GetHashCode():X8}";
+            IActorRef child = Context.Child(childName);
+            if (child.IsNobody())
+            {
+                //start the child if it doesn't exist
+                child = Context.ActorOf(Props.Create(() => 
+                new DocumentWordCounter(s.DocumentId)), childName);
+            }
+            child.Forward(s);
+        });
+    }
+}
